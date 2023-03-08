ccd9d8dcff634e4f9b4a4f0cdcca705cd26291d6
Saydum Khalibekov <saydum.it5@gmail.com>
2023-03-08 13:58:44 +0300

    Update README.md and start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+composer install &&
+npm install &&
+npm build &&
 docker-compose up -d &&
 cp .env.example .env &&
 php artisan migrate &&
-# php artisan db:seed &&
+php artisan db:seed &&
 php artisan serve
