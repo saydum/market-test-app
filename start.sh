83e3a0d240ed62986b02ee8f3d8317e447655ab2
Saydum Khalibekov <saydum.it5@gmail.com>
2023-03-08 16:23:27 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,6 @@ npm install &&
 npm run build &&
 docker-compose up -d &&
 cp .env.example .env &&
-php artisan migrate:refresh &&
-php artisan db:seed &&
+php artisan migrate:refresh --seed &&
 php artisan key:generate &&
 php artisan serve
